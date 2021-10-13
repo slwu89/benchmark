a03d0bf1e01c70219924ea464cfb669f2a1c5615
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-10-13 13:47:40 -0700

    more bugs

--- ranges/R/ranges.R
+++ ranges/R/ranges.R
@@ -1,6 +1,32 @@
 
 
-#' @title add powers
+#' #' @title add powers
+#' #' @export
+#' add_powers <- function(lo, hi, mult = 8) {
+#'   
+#'   lo <- as.integer(lo)
+#'   hi <- as.integer(hi)
+#'   mult <- as.integer(mult)
+#'   
+#'   stopifnot(lo >= 0)
+#'   stopifnot(hi >= lo)
+#'   stopifnot(mult >= 2)
+#'   
+#'   kmax <- .Machine$integer.max
+#'   
+#'   # space out the values in multiples of mult, to get from lo to hi, will have about lo * mult^x = hi, solve for x
+#'   dst <- lo * cumprod(x = c(1, rep(x = mult, each = floor(log(x = hi/lo, base = mult)))))
+#'   
+#'   if (any(dst > kmax)) {
+#'     stop("some elements of generated range larger than greatest representable integer!")
+#'   }
+#'   
+#'   return(as.integer(dst))
+#' }
+
+
+
+#' #' @title add powers
 #' @export
 add_powers <- function(lo, hi, mult = 8) {
   
@@ -15,7 +41,9 @@ add_powers <- function(lo, hi, mult = 8) {
   kmax <- .Machine$integer.max
   
   # space out the values in multiples of mult, to get from lo to hi, will have about lo * mult^x = hi, solve for x
-  dst <- lo * cumprod(x = c(1, rep(x = mult, each = floor(log(x = hi/lo, base = mult)))))
+  start <- ceiling(log(base = mult, x = lo))
+  end <- floor(log(base = mult, x = hi))
+  dst <- mult^(start:end)
   
   if (any(dst > kmax)) {
     stop("some elements of generated range larger than greatest representable integer!")
@@ -23,6 +51,7 @@ add_powers <- function(lo, hi, mult = 8) {
   
   return(as.integer(dst))
 }
+
 
 
 #' @title add negated powers
